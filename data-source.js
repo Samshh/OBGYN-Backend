d44965cfcaaa94d725376382b0643cadcc238fb6
{Samshh} <{samdacs2@gmail.com}>
2025-03-04 23:24:41 +0800

    changed host

--- data-source.js
+++ data-source.js
@@ -2,7 +2,7 @@ const { DataSource } = require("typeorm");
 
 const AppDataSource = new DataSource({
   type: "mysql",
-  host: "localhost",
+  host: "127.0.0.1",
   port: 3306,
   username: "sam",
   password: "Samuel1234",
