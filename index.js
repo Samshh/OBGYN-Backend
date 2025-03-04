dca9623b7395a5494ff9779594f642e5653d65af
{Samshh} <{samdacs2@gmail.com}>
2025-03-05 00:33:34 +0800

    another fix

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const cookieParser = require('cookie-parser');
 const PORT = process.env.PORT || 3000;
 
 app.use(cors({
-  origin: 'https://obgyn-frontend.vercel.app/', // Correct the origin to http
+  origin: 'https://obgyn-frontend.vercel.app', // Correct the origin to http
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true,
